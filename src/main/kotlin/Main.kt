a43458daae53f68d3c48139c103ec0a1d32b0472
therealbluepandabear <justaguyoney@gmail.com>
2022-09-30 10:23:26 +1300

    Change divider char.

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -16,7 +16,7 @@ fun getRumbleSearchResultsForQuery(query: String, page: Int = 1) {
         } else {
             for (element in doc.getElementsByClass("video-listing-entry")) {
                 for (element2 in element.getElementsByClass("video-item--title")) {
-                    println("${element2.text()}'\n${"_".repeat(100)}")
+                    println("${element2.text()}'\n${"-".repeat(100)}")
                 }
             }
         }
